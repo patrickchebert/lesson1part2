9273f6845d7faa194eafa2b4974de128d89142d8
pc <pc@DESKTOP-I9P3TAR>
2017-01-09 14:04:15 -0500

    Add project files.

---
+++ lesson1part2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
